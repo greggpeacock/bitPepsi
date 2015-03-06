7ab784e97f60091c6188d18227fb2a99e01c3603
greggpeacock <gregg.peacock@gmail.com>
2015-03-06 13:16:38 -0800

    - fixed a bug with energize() reading the config delay value s

--- index.js
+++ index.js
@@ -170,9 +170,9 @@ function energize(done, results) {
                             }
                         });
 
-                    },duration); // how long in ms do we keep the GPIO 'high'
+                    }); // how long in ms do we keep the GPIO 'high'
 
-                });
+                },duration);
 
             });
         });
